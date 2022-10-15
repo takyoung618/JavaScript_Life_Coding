a7adfdf21a058dda75482ce14d9ae84e81b164b8
takyoung618 <wisdom8751@gmail.com>
2022-10-15 14:26:39 +0900

    1-4 문자에 대한 명령들

---
+++ 01_숫자와 문자/1-4 문자에 대한 명령들.js
@@ -0,0 +1,9 @@
+// 숫자를 더하는 것과 문자를 더하는 것은 다르다.
+1 + 1 //2
+"1" + "1" //"11"
+
+// indexOf: 문자열 내에서 index(특정한 문자열의 위치) 값을 리턴한다.
+"code".indexOf("c") // 0
+"code".indexOf("o") // 1
+"code".indexOf("d") // 2
+"code".indexOf("e") // 3
