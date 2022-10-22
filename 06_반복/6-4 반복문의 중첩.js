f949917a3df2bb5f3eda10a39a8fcab94d15e04e
takyoung618 <wisdom8751@gmail.com>
2022-10-22 21:39:33 +0900

    6-4 반복문의 중첩

---
+++ 06_반복/6-4 반복문의 중첩.js
@@ -0,0 +1,8 @@
+// 반복문의 중첩
+for(let i =0; i<10; i++){
+    for(let j =0; j<10; j++){
+        document.write("Coding everybody "+i+j+"<br />");
+    }
+}
+// 출력하면 01, 02, 03...10, 11, 12...20...99까지의 값이 출력된다.
+   
