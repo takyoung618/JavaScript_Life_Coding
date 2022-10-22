060099cb0c43d1eca69ca21a8041ee6fb5b3fccc
takyoung618 <wisdom8751@gmail.com>
2022-10-22 19:31:53 +0900

    5-3 조건문의 응용

--- 05_조건문/5-3 조건문의 응용.js
+++ 05_조건문/5-3 조건문의 응용.js
@@ -8,7 +8,7 @@ prompt('당신의 나이는?');
 let id = prompt('아이디를 입력해주세요.')
 if(id == 'egoing'){
     let password = prompt('비밀번호를 입력해주세요.')
-    if(password == '111111'){
+    if(password == '11111'){
         alert('로그인 하셨습니다.')
     } else {
         alert('비밀번호가 다릅니다.')
