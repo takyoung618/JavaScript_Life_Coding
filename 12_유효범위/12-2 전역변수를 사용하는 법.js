54cbb80904a21b35a24becdae9b9aaf75a7194d2
takyoung618 <wisdom8751@gmail.com>
2022-11-07 12:24:59 +0900

    12-2

---
+++ 12_유효범위/12-2 전역변수를 사용하는 법.js
@@ -0,0 +1,21 @@
+// function으로 감싸주면 MYAPP은 전역변수가 아닌 함수의 지역변수가 된다.
+// 함수를 정의하고 그것을 바로 호출하는 다음과 같은 기법을 익명함수라고 한다.
+
+(function(){
+    var MYAPP = {}
+    MYAPP.calculator = {
+        'left' : null,
+        'right' : null
+    }
+    MYAPP.coordinate = {
+        'left' : null,
+        'right' : null
+    }
+
+    MYAPP.calculator.left = 10;
+    MYAPP.coordinate.right = 20;
+    function sum(){
+        return MYAPP.calculator.left + MYAPP.coordinate.right;
+    }
+    document.write(sum());
+}())    
